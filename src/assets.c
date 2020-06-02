84cdda5c3acfd60458b1d2d922f6a28585e75915
lixianjing <xianjimli@hotmail.com>
2020-06-02 18:11:49 +0800

    update asets

--- src/assets.c
+++ src/assets.c
@@ -1,104 +1,80 @@
 #include "awtk.h"
 #include "base/assets_manager.h"
-#ifndef WITH_FS_RES
-#include "assets/inc/strings/zh_CN.data"
-#include "assets/inc/strings/en_US.data"
-#include "assets/inc/styles/default.data"
-#include "assets/inc/ui/calculator.data"
-#ifdef WITH_STB_IMAGE
-#include "assets/inc/images/checked.res"
-#include "assets/inc/images/earth.res"
-#include "assets/inc/images/arrow_up_n.res"
-#include "assets/inc/images/arrow_right_p.res"
-#include "assets/inc/images/empty.res"
-#include "assets/inc/images/edit_clear_p.res"
-#include "assets/inc/images/arrow_up_o.res"
-#include "assets/inc/images/arrow_left_o.res"
-#include "assets/inc/images/arrow_left_n.res"
-#include "assets/inc/images/radio_checked.res"
-#include "assets/inc/images/check.res"
-#include "assets/inc/images/arrow_down_n.res"
-#include "assets/inc/images/arrow_down_o.res"
-#include "assets/inc/images/unchecked.res"
-#include "assets/inc/images/arrow_down_p.res"
-#include "assets/inc/images/radio_unchecked.res"
-#include "assets/inc/images/arrow_up_p.res"
-#include "assets/inc/images/edit_clear_o.res"
-#include "assets/inc/images/arrow_right_n.res"
-#include "assets/inc/images/arrow_right_o.res"
-#include "assets/inc/images/edit_clear_n.res"
-#include "assets/inc/images/arrow_left_p.res"
-#else
-#include "assets/inc/images/arrow_right_o.data"
-#include "assets/inc/images/earth.data"
-#include "assets/inc/images/arrow_up_p.data"
-#include "assets/inc/images/arrow_right_n.data"
-#include "assets/inc/images/checked.data"
-#include "assets/inc/images/arrow_right_p.data"
-#include "assets/inc/images/arrow_up_o.data"
-#include "assets/inc/images/empty.data"
-#include "assets/inc/images/radio_unchecked.data"
-#include "assets/inc/images/arrow_up_n.data"
-#include "assets/inc/images/arrow_left_p.data"
-#include "assets/inc/images/edit_clear_n.data"
-#include "assets/inc/images/arrow_down_p.data"
-#include "assets/inc/images/edit_clear_o.data"
-#include "assets/inc/images/arrow_left_o.data"
-#include "assets/inc/images/check.data"
-#include "assets/inc/images/arrow_down_o.data"
-#include "assets/inc/images/radio_checked.data"
-#include "assets/inc/images/unchecked.data"
-#include "assets/inc/images/arrow_down_n.data"
-#include "assets/inc/images/arrow_left_n.data"
-#include "assets/inc/images/edit_clear_p.data"
-#endif /*WITH_STB_IMAGE*/
-#ifdef WITH_VGCANVAS
-#endif /*WITH_VGCANVAS*/
-#if defined(WITH_STB_FONT) || defined(WITH_FT_FONT)
-#include "assets/inc/fonts/default.res"
-#else /*WITH_STB_FONT or WITH_FT_FONT*/
-#include "assets/inc/fonts/default.data"
-#endif /*WITH_STB_FONT or WITH_FT_FONT*/
-#endif /*WITH_FS_RES*/
+
+#include "assets_default.inc"
+
+#ifndef APP_THEME
+#define APP_THEME "default"
+#endif /*APP_THEME*/
+
+bool_t assets_has_theme(const char* name) {
+  return_value_if_fail(name != NULL, FALSE);
+
+  if (tk_str_eq(name, "default")) {
+    return TRUE;
+  } else {
+    return FALSE;
+  }
+}
+
+static ret_t assets_init_internal(const char* theme) {
+  assets_manager_t* am = assets_manager();
+  return_value_if_fail(theme != NULL && am != NULL, RET_BAD_PARAMS);
+
+  assets_manager_set_theme(am, theme);
+
+  if (tk_str_eq(theme, "default")) {
+    return assets_init_default();
+  } else {
+    log_debug("%s not support.\n", theme);
+    return RET_NOT_IMPL;
+  }
+}
 
 ret_t assets_init(void) {
-  assets_manager_t* rm = assets_manager();
+  return assets_init_internal(APP_THEME);
+}
 
-#ifdef WITH_FS_RES
-  assets_manager_preload(rm, ASSET_TYPE_FONT, "default");
-  assets_manager_preload(rm, ASSET_TYPE_STYLE, "default");
-#else
-  assets_manager_add(rm, ui_calculator);
-  assets_manager_add(rm, strings_zh_CN);
-  assets_manager_add(rm, strings_en_US);
-  assets_manager_add(rm, image_arrow_right_o);
-  assets_manager_add(rm, image_earth);
-  assets_manager_add(rm, image_arrow_up_p);
-  assets_manager_add(rm, image_arrow_right_n);
-  assets_manager_add(rm, image_checked);
-  assets_manager_add(rm, image_arrow_right_p);
-  assets_manager_add(rm, image_arrow_up_o);
-  assets_manager_add(rm, image_empty);
-  assets_manager_add(rm, image_radio_unchecked);
-  assets_manager_add(rm, image_arrow_up_n);
-  assets_manager_add(rm, image_arrow_left_p);
-  assets_manager_add(rm, image_edit_clear_n);
-  assets_manager_add(rm, image_arrow_down_p);
-  assets_manager_add(rm, image_edit_clear_o);
-  assets_manager_add(rm, image_arrow_left_o);
-  assets_manager_add(rm, image_check);
-  assets_manager_add(rm, image_arrow_down_o);
-  assets_manager_add(rm, image_radio_checked);
-  assets_manager_add(rm, image_unchecked);
-  assets_manager_add(rm, image_arrow_down_n);
-  assets_manager_add(rm, image_arrow_left_n);
-  assets_manager_add(rm, image_edit_clear_p);
-  assets_manager_add(rm, style_default);
-  assets_manager_add(rm, font_default);
-#ifdef WITH_VGCANVAS
-#endif /*WITH_VGCANVAS*/
-#endif
+static ret_t widget_set_theme_without_file_system(widget_t* widget, const char* name) {
+#ifndef WITH_FS_RES
+  const asset_info_t* info = NULL;
+  event_t e = event_init(EVT_THEME_CHANGED, NULL);
+  widget_t* wm = widget_get_window_manager(widget);
+  font_manager_t* fm = widget_get_font_manager(widget);
+  image_manager_t* imm = widget_get_image_manager(widget);
+  assets_manager_t* am = widget_get_assets_manager(widget);
+  locale_info_t* locale_info = widget_get_locale_info(widget);
 
-  tk_init_assets();
+  return_value_if_fail(am != NULL && name != NULL, RET_BAD_PARAMS);
+  return_value_if_fail(assets_has_theme(name), RET_BAD_PARAMS);
+
+  font_manager_unload_all(fm);
+  image_manager_unload_all(imm);
+  assets_manager_clear_all(am);
+  widget_reset_canvas(widget);
+
+  assets_init_internal(name);
+  locale_info_reload(locale_info);
+
+  info = assets_manager_ref(am, ASSET_TYPE_STYLE, "default");
+  theme_init(theme(), info->data);
+  assets_manager_unref(assets_manager(), info);
+
+  widget_dispatch(wm, &e);
+  widget_invalidate_force(wm, NULL);
+
+  log_debug("theme changed: %s\n", name);
+
   return RET_OK;
+#else /*WITH_FS_RES*/
+  return RET_NOT_IMPL;
+#endif /*WITH_FS_RES*/
+}
+
+ret_t assets_set_global_theme(const char* name) {
+#ifdef WITH_FS_RES
+  return widget_set_theme(window_manager(), name);
+#else  /*WITH_FS_RES*/
+  return widget_set_theme_without_file_system(window_manager(), name);
+#endif /*WITH_FS_RES*/
 }
